afaf373c65a69a8f0a864d9b5565b7eb0a53a506
Salvador Toscano <stoscano3@student.rccd.edu>
2024-09-13 15:24:42 -0700

    Signed-off-by: Salvador Toscano <stoscano3@student.rccd.edu>

---
+++ Hello_World/Hello_World.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Hello_World.cpp
+ * Author: shpctac1002f
+ *
+ * Created on September 13, 2024, 3:21 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
